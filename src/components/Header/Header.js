71154cc5b01366e222a1fefd7ddceea397700e46
ptmartins <pedro@pedromartins.com.pt>
2024-06-09 20:10:57 +0100

    Add Logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,25 @@
+'use client';
+
 import { FiLogOut } from "react-icons/fi";
 import styles from './Header.module.css';
 
-const Header = ({ version, user, loggedIn }) => {
+const Header = ({ version, user }) => {
+
+    const loggedIn = localStorage.getItem('mccLoggedIn');
+
+    const handleLogout = () => {
+        localStorage.setItem('mccLoggedIn', false);
+        window.location = '/';
+    }
+
     return(
         <header className={ styles.header }>
             <h1>Media Control Centre <span className={ styles.version }> version 7.1.1.872 </span> </h1>
 
             { loggedIn &&
                 <>
-                    <span className={styles.welcome}> { welcome } </span> 
-                    <a href="/" className={`u-link ${ styles.logout }`} title="Logout" > <FiLogOut /> </a>
+                    <span className={styles.welcome}> Welcome, Admin. </span> 
+                    <button className={`btn ${ styles.logout }`} title="Logout" onClick={ handleLogout }> <FiLogOut /> </button>
                 </>
             }
         </header>    
